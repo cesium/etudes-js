b36ad0621c8d094d540d675f2e7755d048b25b13
11GG20 <fl11gg20@gmail.com>
2024-11-19 23:42:49 +0000

    terceiro capítulo

--- Chapter3/Exercise11/minimo.js
+++ Chapter3/Exercise11/minimo.js
@@ -1,6 +1,19 @@
 //Escreve uma função que encontre o menor número em um array de inteiros.
 
-function minimoSimples(arr) {}
+function minimoSimples(arr) {
+    let min = 1000000
+    for (item of arr) {
+        if (min>item) {
+            min = item
+        }
+    } return min
+}
+
+// ou
+
+function minimoSimples2(arr) {
+    return Math.min(...arr)
+}
 
 let numeros = [10, 5, 8, 1, 7];
-console.log("Menor número:", minimoSimples(numeros)); //output: 1
+console.log("Menor número:", minimoSimples2(numeros)); //output: 1
--- Chapter3/Exercise12/removerDuplicados.js
+++ Chapter3/Exercise12/removerDuplicados.js
@@ -1,6 +1,13 @@
 // Escreve uma função que remova todos os elementos duplicados de um array.
 
-function removeDuplicados(arr) {}
+function removeDuplicados(arr) {
+    let arrF = []
+    for (item of arr) {
+        if (!arrF.includes(item)) {
+            arrF.push(item)
+        }
+    } return arrF
+} 
 
 let arrDuplicados = [1, 2, 3, 2, 1, 4, 5];
 console.log("Array sem duplicatas:", removeDuplicados(arrDuplicados)); //output: [ 1, 2, 3, 4, 5 ]
