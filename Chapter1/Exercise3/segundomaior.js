63c3a52b694f2b45186a866f81a76994f5cab76d
11GG20 <fl11gg20@gmail.com>
2024-11-19 23:37:49 +0000

    primiero capítulo

--- Chapter1/Exercise3/segundomaior.js
+++ Chapter1/Exercise3/segundomaior.js
@@ -1,6 +1,9 @@
 // Escreve uma função que encontre o segundo maior número em um array de inteiros
 
-function segundoMaior(numeros) {}
+function segundoMaior(numero) {
+    let numerosOrd = numero.sort()
+    return numerosOrd[(numero.length)-2]
+}
 
 let numeros = [3, 5, 7, 2, 8];
 console.log("Segundo maior número:", segundoMaior(numeros)); //output: 7
