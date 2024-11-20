c8707d301e6af4283815b304f6481d796b28f95f
11GG20 <fl11gg20@gmail.com>
2024-11-20 01:20:32 +0000

    quinto capítulo

--- Chapter5/Exercise21/ordenarArray.js
+++ Chapter5/Exercise21/ordenarArray.js
@@ -1,5 +1,7 @@
 // Implementa uma função para ordenar um array de números em ordem crescente.
 
-function ordenarArray(nums) {}
+function ordenarArray(nums) {
+    return nums.sort()
+}
 
 console.log(ordenarArray([4, 2, 7, 1, 9])); //output: [1, 2, 4, 7, 9]
--- Chapter5/Exercise23/maiorNumeroComum.js
+++ Chapter5/Exercise23/maiorNumeroComum.js
@@ -1,6 +1,15 @@
 // Dados dois arrays de números, retorna o maior número que esteja presente em ambos os arrays. Se não houver número comum, retorne -1.
 
-function maiorNumeroComum(arr1, arr2) {}
+function maiorNumeroComum(arr1, arr2) {
+    let max = 0
+    for (item of arr1) {
+        if (arr2.includes(item)) {
+            if (max < item) {
+                max = item
+            }
+        }
+    } return max || -1
+}
 
 console.log(maiorNumeroComum([1, 5, 10, 20], [3, 5, 10, 30])); //output: 10
 console.log(maiorNumeroComum([1, 2, 3], [4, 5, 6])); //output: -1
